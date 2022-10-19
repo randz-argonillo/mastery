77479461d6090861357defe5d55f02e685c2bd62
randy <randz.argonillo@gmail.com>
2022-10-20 00:39:42 +0800

    Flesh out core functions

--- lib/mastery/core/response.ex
+++ lib/mastery/core/response.ex
@@ -2,7 +2,23 @@ defmodule Mastery.Core.Response do
   defstruct quiz_title: '',
     template_name: '',
     to: '',
+    email: '',
     answer: '',
     correct: false,
     timestamp: nil
+
+  def new(quiz, email, answer) do
+    question = quiz.current_question
+    template = question.template
+
+    %__MODULE__{
+      quiz_title: quiz.title,
+      template_name: template.name,
+      to: question.asked,
+      email: email,
+      answer: answer,
+      correct: template.checker.(question.substitutions, answer),
+      timestamp: DateTime.utc_now()
+    }
+  end
 end
--- lib/mastery/core/template.ex
+++ lib/mastery/core/template.ex
@@ -1,3 +1,9 @@
 defmodule Mastery.Core.Template do
   defstruct ~w[name category instructions raw compiled generators checker]a
+
+  def new(fields) do
+    raw = Keyword.fetch!(fields, :raw)
+    compiled = EEx.compile_string(raw)
+    struct!(__MODULE__, Keyword.put(fields, :compiled, compiled))
+  end
 end
